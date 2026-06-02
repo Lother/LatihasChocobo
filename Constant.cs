cf0fdf8b6b4fa5cea441faeb74136184c999ed4c
Latihas <lyjp2002@163.com>
2026-06-02 21:25:00 +0800

    7.50.2.0

--- Constant.cs
+++ Constant.cs
@@ -2,5 +2,5 @@
 
 public static class Constant {
 	public const uint WM_KEYUP = 0x101, WM_KEYDOWN = 0x100;
-	public const byte OPCODE_DUTY_1 = 0x98, OPCODE_DUTY_2 = 0x02;
+	public const byte OPCODE_DUTY_1 = 0xF0, OPCODE_DUTY_2 = 0x01;
 }
